b796127599b416c85268f993b0cd7d3740f9a605
Christelle SODJINOU <yisegnon.sodjinou.07@gmail.com>
2026-05-01 18:17:59 +0100

    Update votes-config.js

--- votes-config.js
+++ votes-config.js
@@ -2,20 +2,20 @@
 (function () {
     const rawVotes = {
         1: 0, // OUSSOU Anais
-        2: 0, // DOS SANTOS Marie-Belle
+        2: 1, // DOS SANTOS Marie-Belle
         3: 0, // BIYAOU Fortunelle
         4: 0, // KINTOGANDOU Vanessa
         5: 0, // CHABI-BOUKARI Zahidath
-        6: 0, // Kevine DONTE
-        7: 0, // LOKO Ingrid
-        8: 0, // NONWANON Eurielle
+        6: 1, // Kevine DONTE
+        7: 21, // LOKO Ingrid
+        8: 1, // NONWANON Eurielle
         9: 0, // AMOUSSOU Isnelle
-        10: 0, // ADAM-TOURE Amiratou
+        10: 1, // ADAM-TOURE Amiratou
         11: 0, // HOUENOU Sephora
-        12: 0, // AKOTENOU Octavie
-        13: 0, // HOUNYO Ornella
+        12: 2, // AKOTENOU Octavie
+        13: 2, // HOUNYO Ornella
         14: 0, // KEDOTE Shammel
-        15: 0 // BIYAOU Fortunelle
+        15: 106, // BIYAOU Fortunelle
     };
 
     window.MANUAL_VALIDATED_VOTES = Object.entries(rawVotes).reduce((safeVotes, [candidateId, value]) => {
